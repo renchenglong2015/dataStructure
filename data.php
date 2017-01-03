13143ea5f7f18841727b5d794a763c15bfe68f60
renchenglong2015 <523465560@qq.com>
2017-01-03 14:45:11 +0800

    二分查找

--- data.php
+++ data.php
@@ -1,8 +1,28 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$data = array(4, 6, 7, 8, 14, 55, 67, 145, 218, 237, 284);
 
+$num = binarysearch(284);
+var_dump($num);
+
+function binarysearch($num)
+{
+    global $data;
+    $count = count($data);
+    $high = $count - 1;
+    $low = 0;
+
+    while ($high >= $low) {
+        $mid = floor(($high + $low) / 2);
+        if ($num == $data[$mid]) {
+            return $mid;
+        } elseif ($num > $data[$mid]) {
+            $low = $mid + 1;
+        } elseif ($num < $data[$mid]) {
+            $high = $mid - 1;
+        }
+    }
+    return false;
+}
+
+?>
